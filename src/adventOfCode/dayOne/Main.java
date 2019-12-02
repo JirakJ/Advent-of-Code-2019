1526dfe1aac225543e349bed45ae616e15c4d366
Jakub Jirák <jakub@jakubjirak.com>
2019-12-03 00:20:43 +0100

    Add Day 1 solution

---
+++ src/adventOfCode/dayOne/Main.java
@@ -0,0 +1,10 @@
+package adventOfCode.dayOne;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String filePath = "src/resources/dayOneInput.txt";
+        System.out.println("Module part 1: "+Module.part1(filePath));
+        System.out.println("Module part 2: "+Module.part2(filePath));
+    }
+}
---
+++ src/adventOfCode/dayOne/Module.java
@@ -0,0 +1,63 @@
+package adventOfCode.dayOne;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+class Module {
+
+    static int part1(String filePath) {
+        List<Integer> input = new LinkedList<>();
+        int result = 0;
+
+        try (Stream<String> stream = Files.lines(Paths.get(filePath))) {
+            input = stream
+                    .map(Integer::valueOf)
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (int current : input) {
+            result += Math.floorDiv(current, 3) - 2;
+        }
+
+//        System.out.println(result);
+
+        return result;
+    }
+
+    static int part2(String filePath) {
+        List<Integer> input = new LinkedList<>();
+        int result = 0;
+
+        try (Stream<String> stream = Files.lines(Paths.get(filePath))) {
+            input = stream
+                    .map(Integer::valueOf)
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (int current : input) {
+            result += calculateFuel(current);
+        }
+
+//        System.out.println(result);
+
+        return result;
+    }
+
+    private static int calculateFuel(int current) {
+        if (current <= 8) return 0;
+        else {
+            int result = Math.floorDiv(current, 3) - 2;
+            return result + calculateFuel(result);
+        }
+    }
+}
