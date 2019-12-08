3d1e9d916f59715498916a274cabf93d8d78085d
Jakub Jirák <jakub@jakubjirak.com>
2019-12-08 10:44:00 +0100

    Update gitignore file.

---
+++ src/adventOfCode/day7/cmake-build-debug/CMakeFiles/AoCDay7.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AoCDay7.dir/Main.cpp.o"
+  "AoCDay7.pdb"
+  "AoCDay7"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AoCDay7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
