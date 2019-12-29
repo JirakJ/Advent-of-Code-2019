e888dd9f2a773d6a3b042335fcc6c833f592d982
Jakub Jirák <jakub@jakubjirak.com>
2019-12-29 02:24:49 +0100

    Add solution for day 9

---
+++ src/adventOfCode/day09/part2/Main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <list>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <algorithm>
+#include <math.h>
+#include <stack>
+#include <bitset>
+#include "../helper.hpp"
+
+int main()
+{
+	std::fstream in("input.in", std::fstream::in);
+	std::fstream out("output.out", std::fstream::out);
+	std::vector<long long> integers;
+	long long input = 2;
+
+	readInput(in, integers);
+	intCodeProgram(out, integers, input);
+
+	in.close();
+	out.close();
+}
